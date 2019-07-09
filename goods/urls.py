1fcabbef6aa7d3a222471dddf424c6170d8c6f6d
zh <zhawyt@163.com>
2019-07-09 14:18:48 +0800

    today暂存

--- goods/urls.py
+++ goods/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'^get_price/(?P<spu_id>\d+)$', views.get_price, name='get_price'),
     url(r'^list_page/(?P<cid>\d+)$', views.list_page, name='list_page'),
     url(r'^spu_filter/(?P<cid>\d+)$', views.spu_filter, name='spu_filter'),
+    url(r'^cart_manage/$', views.cart_manage, name='cart_manage'),
 ]
